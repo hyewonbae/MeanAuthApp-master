206c339cd10d3633ec549a26a32a8feef615f4b5
seyoung7008 <31922480+seyoung7008@users.noreply.github.com>
2018-11-29 15:50:31 +0900

    url 변경
    베이스 url을 세영이 도메인으로 변경해서 cmder로 먼저 돌리지 않아도 푸티에서 돌아가게 변경

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -17,20 +17,20 @@ export class AuthService {
   registerNewu(newu) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/newus/register', newu, {headers: headers})
+    return this.http.post('http://13.209.244.98:3000/newus/register', newu, {headers: headers})
       .pipe(map(res => res.json()));
   }
   addStore(store) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/stores/addstore', store, {headers: headers})
+    return this.http.post('http://13.209.244.98:3000/stores/addstore', store, {headers: headers})
       .pipe(map(res => res.json()));
   }
 
   authenticateUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+    return this.http.post('http://13.209.244.98:3000/users/authenticate', user, {headers: headers})
       .pipe(map(res => res.json()));
   }
 
@@ -39,7 +39,7 @@ export class AuthService {
     this.loadToken();
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type', 'application/json');
-    return this.http.get('http://localhost:3000/newus/profile', {headers: headers})
+    return this.http.get('http://13.209.244.98:3000/newus/profile', {headers: headers})
       .pipe(map(res => res.json()));
   }
 
