070148713a0df3c1d2dbe9d0f44c13283a99708f
hyewonbae <bhjn8991@naver.com>
2018-12-11 02:55:03 +0900

    오늘 마지막
    수정끝

--- public/main.js
+++ public/main.js
@@ -41,7 +41,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--The content below is only a placeholder and can be replaced.-->\r\n<app-navbar></app-navbar>\r\n<div class=\"container\">\r\n  <ng-flash-message></ng-flash-message>\r\n  <router-outlet></router-outlet>\r\n</div>\r\n"
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<app-navbar></app-navbar>\n<div class=\"container\">\n  <ng-flash-message></ng-flash-message>\n  <router-outlet></router-outlet>\n</div>\n"
 
 /***/ }),
 
@@ -317,7 +317,7 @@ module.exports = "h1{\r\n    color:rgb(115, 133, 153);\r\n    font-family: 'Cour
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"jumbotron text-center\">\r\n  <h1><b>Welcome to Quiclick!</b></h1>\r\n  <img width=\"300\" src=\"https://img.lovepik.com/element/40053/2679.png_1200.png\">\r\n  <div class=\"row\">\r\n      <div class=\"col-md-4\">\r\n        <h5>COMPANY INFO</h5>\r\n        <p>company:(주)quiclick</p>\r\n        <P>Email:BSJ@hansung.ac.kr</P>\r\n        <p>TEL:1234-5678</p>\r\n      </div>\r\n      <div class=\"col-md-4\">\r\n        <h5>QUICLICK</h5>\r\n        <p>We offer you the best restaurant reservation service :)</p>\r\n        <p>We will always strive for better service.</p>\r\n      </div>\r\n      <div class=\"col-md-4\">\r\n        <h5>App</h5>\r\n        <p>Quiclick App</p><img width=\"30\" src=\"https://img.lovepik.com/element/40053/2679.png_1200.png\">\r\n      </div>\r\n    </div>\r\n</div>\r\n"
+module.exports = "<div class=\"jumbotron text-center\">\n  <h1><b>Welcome to Quiclick!</b></h1>\n  <img width=\"300\" src=\"https://img.lovepik.com/element/40053/2679.png_1200.png\">\n  <div class=\"row\">\n      <div class=\"col-md-4\">\n        <h5>COMPANY INFO</h5>\n        <p>company:(주)quiclick</p>\n        <P>Email:BSJ@hansung.ac.kr</P>\n        <p>TEL:1234-5678</p>\n      </div>\n      <div class=\"col-md-4\">\n        <h5>QUICLICK</h5>\n        <p>We offer you the best restaurant reservation service :)</p>\n        <p>We will always strive for better service.</p>\n      </div>\n      <div class=\"col-md-4\">\n        <h5>App</h5>\n        <p>Quiclick App</p><img width=\"30\" src=\"https://img.lovepik.com/element/40053/2679.png_1200.png\">\n      </div>\n    </div>\n</div>\n"
 
 /***/ }),
 
@@ -443,7 +443,7 @@ module.exports = "h1{\r\n    color:rgb(115, 133, 153);\r\n    font-family: 'Cour
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"jumbotron text-center\" style=\" margin:0 auto; width:930px; height:550px; vertical-align:middle\">\r\n  <h1><b>Welcome to Quiclick!</b></h1>\r\n  <img width=\"300\" src=\"https://img.lovepik.com/element/40053/2679.png_1200.png\">\r\n  <div>\r\n    <br>\r\n    <a class=\"btn btn-secondary\" [routerLink]=\"['/register']\">Register</a> &nbsp; \r\n    <a class=\"btn btn-secondary\" [routerLink]=\"['/login']\">AdminLogin</a>\r\n  </div>\r\n</div>\r\n"
+module.exports = "<div class=\"jumbotron text-center\" style=\" margin:0 auto; width:930px; height:550px; vertical-align:middle\">\n  <h1><b>Welcome to Quiclick!</b></h1>\n  <img width=\"300\" src=\"https://img.lovepik.com/element/40053/2679.png_1200.png\">\n  <div>\n    <br>\n    <a class=\"btn btn-secondary\" [routerLink]=\"['/register']\">Register</a> &nbsp; \n    <a class=\"btn btn-secondary\" [routerLink]=\"['/login']\">AdminLogin</a>\n  </div>\n</div>\n"
 
 /***/ }),
 
@@ -506,7 +506,7 @@ module.exports = "h2{\r\n    color:rgb(115, 133, 153);\r\n    font-family: 'Cour
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"jumbotron text-center\" style=\" margin:0 auto; width:500px; height:300px; vertical-align:middle\">\r\n<h2 class=\"page-header\"><b>Login</b></h2>\r\n<form (submit)=\"onLoginSubmit()\">\r\n  <div class=\"form-group\">\r\n   \r\n    <input type=\"text\" class=\"form-control\" [(ngModel)]='username' name=\"username\" placeholder=\"Username\">\r\n  </div>\r\n  <div class=\"form-group\">\r\n    \r\n    <input type=\"password\" class=\"form-control\" [(ngModel)]='password' name=\"password\" placeholder=\"Password\">\r\n  </div>\r\n  <input type=\"submit\" class=\"btn btn-secondary\" value=\"Login\">\r\n</form>\r\n</div>"
+module.exports = "<div class=\"jumbotron text-center\" style=\" margin:0 auto; width:500px; height:300px; vertical-align:middle\">\n<h2 class=\"page-header\"><b>Login</b></h2>\n<form (submit)=\"onLoginSubmit()\">\n  <div class=\"form-group\">\n   \n    <input type=\"text\" class=\"form-control\" [(ngModel)]='username' name=\"username\" placeholder=\"Username\">\n  </div>\n  <div class=\"form-group\">\n    \n    <input type=\"password\" class=\"form-control\" [(ngModel)]='password' name=\"password\" placeholder=\"Password\">\n  </div>\n  <input type=\"submit\" class=\"btn btn-secondary\" value=\"Login\">\n</form>\n</div>"
 
 /***/ }),
 
@@ -693,7 +693,7 @@ module.exports = "table {\r\n  border-collapse: collapse;\r\n  width: 100%;\r\n}
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<form (submit)=\"deleteusers()\">\r\n    <div class=\"container\">\r\n      <h3>Userlist</h3>      \r\n      <table class=\"table table-condensed\" >\r\n        <thead>\r\n          <tr>\r\n            <th style=\"text-align: center\" width=\"20%\">No</th>\r\n            <th style=\"text-align: center\" width=\"30%\">ID</th>\r\n            <th style=\"text-align: center\" width=\"40%\">USERNAME</th>\r\n            <th style=\"text-align: center\" width=\"10%\">Delete</th>\r\n          </tr>\r\n        </thead>\r\n        <tbody>\r\n          <tr *ngFor=\"let user of users; let i=index\" style=\"text-align:center\" onMouseOver=\"this.className='bg1'\" onMouseOut=\"this.className='bg2'\">\r\n            <th style=\"text-align: center\" width=\"20%\">{{i+1}}</th>\r\n            <th style=\"text-align: center\" width=\"40%\">{{user.name}}</th>\r\n            <th style=\"text-align: center\" width=\"40%\">{{user.username}}</th>\r\n            <th style=\"text-align: center\" width=\"20%\"><input type=\"submit\"  value=\"X\"> </th>\r\n          </tr>\r\n       </tbody>\r\n       </table>\r\n       </div>\r\n      </form>"
+module.exports = "<form (submit)=\"deleteusers()\">\r\n    <div class=\"container\">\r\n      <h3>Userlist</h3>      \r\n      <table class=\"table table-condensed\" >\r\n        <thead>\r\n          <tr>\r\n            <th style=\"text-align: center\" width=\"10%\">No</th>\r\n            <th style=\"text-align: center\" width=\"40%\">ID</th>\r\n            <th style=\"text-align: center\" width=\"40%\">USERNAME</th>\r\n            <th style=\"text-align: center\" width=\"10%\">Delete</th>\r\n          </tr>\r\n        </thead>\r\n        <tbody>\r\n          <tr *ngFor=\"let user of users; let i=index\" style=\"text-align:center\" onMouseOver=\"this.className='bg1'\" onMouseOut=\"this.className='bg2'\">\r\n            <th style=\"text-align: center\" width=\"10%\">{{i+1}}</th>\r\n            <th style=\"text-align: center\" width=\"40%\">{{user.name}}</th>\r\n            <th style=\"text-align: center\" width=\"40%\">{{user.username}}</th>\r\n            <th style=\"text-align: center\" width=\"10%\"><input type=\"submit\"  value=\"X\"> </th>\r\n          </tr>\r\n       </tbody>\r\n       </table>\r\n       </div>\r\n      </form>"
 
 /***/ }),
 
@@ -783,7 +783,7 @@ module.exports = "h2{\r\n    color:rgb(115, 133, 153);\r\n    font-family: 'Cour
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"jumbotron text-center\" style=\" margin:0 auto; width:500px; height:400px; vertical-align:middle\">\r\n<h2 class=\"page-header\"><b> Register </b></h2>\r\n<form (submit)=\"onRegisterSubmit()\">\r\n    \r\n  <div class=\"form-group\" >\r\n  \r\n    <input type=\"text\" [(ngModel)]=\"name\" name=\"name\" class=\"form-control\" placeholder=\"ID\">\r\n  </div>\r\n  <div class=\"form-group\">\r\n    \r\n    <input type=\"text\" [(ngModel)]=\"username\" name=\"username\"  class=\"form-control\" placeholder=\"Username\">\r\n  </div>\r\n  <div class=\"form-group\">\r\n    \r\n    <!-- <input type=\"text\" [(ngModel)]=\"email\" name=\"email\" class=\"form-control\" placeholder=\"Email\">\r\n  </div>\r\n  <div class=\"form-group\"> -->\r\n\r\n    <input type=\"password\" [(ngModel)]=\"password\" name=\"password\" class=\"form-control\" placeholder=\"Password\">\r\n  </div>\r\n    <input type=\"submit\" class=\"btn btn-primary\" value=\"Submit\">\r\n    \r\n</form>\r\n</div>"
+module.exports = "<div class=\"jumbotron text-center\" style=\" margin:0 auto; width:500px; height:400px; vertical-align:middle\">\n<h2 class=\"page-header\"><b> Register </b></h2>\n<form (submit)=\"onRegisterSubmit()\">\n    \n  <div class=\"form-group\" >\n  \n    <input type=\"text\" [(ngModel)]=\"name\" name=\"name\" class=\"form-control\" placeholder=\"ID\">\n  </div>\n  <div class=\"form-group\">\n    \n    <input type=\"text\" [(ngModel)]=\"username\" name=\"username\"  class=\"form-control\" placeholder=\"Username\">\n  </div>\n  <div class=\"form-group\">\n    \n    <!-- <input type=\"text\" [(ngModel)]=\"email\" name=\"email\" class=\"form-control\" placeholder=\"Email\">\n  </div>\n  <div class=\"form-group\"> -->\n\n    <input type=\"password\" [(ngModel)]=\"password\" name=\"password\" class=\"form-control\" placeholder=\"Password\">\n  </div>\n    <input type=\"submit\" class=\"btn btn-primary\" value=\"Submit\">\n    \n</form>\n</div>"
 
 /***/ }),
 
@@ -899,7 +899,7 @@ module.exports = "/*table {\r\n    border-collapse: collapse;\r\n    width: 100%
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<form (submit)=\"deletereviews()\">\r\n<table class=\"table table-striped\">\r\n  <thead>\r\n      <tr>\r\n        <th style=\"text-align: center\" width=\"15%\">번호</th>\r\n        <th style=\"text-align: center\" width=\"25%\">가게명</th>\r\n        <th style=\"text-align: center\" width=\"60%\">내용</th>\r\n        <th style=\"text-align: center\" width=\"10%\">삭제</th>\r\n      \r\n      </tr>\r\n    </thead>\r\n    <tbody>\r\n     <tr class=\"reviews\" *ngFor=\"let review of reviews; let i =index\">\r\n        <th style=\"text-align: center\" width=\"15%\"> \r\n          <span class=\"badge\">{{i+1}}</span></th>\r\n        <th style=\"text-align: center\" width=\"25%\">{{review.name}}</th>\r\n        <th style=\"text-align: center\" width=\"60%\">{{review.des}}</th>\r\n        <th style=\"text-align: center\" width=\"10%\"> <input type=\"submit\"  value=\"delete\"></th>\r\n      </tr>\r\n    </tbody>\r\n</table>\r\n\r\n\r\n   &nbsp; <a class=\"btn btn-secondary\">edit</a>\r\n    </form>\r\n    \r\n    \r\n     \r\n      "
+module.exports = "<form (submit)=\"deletereviews()\">\r\n<table class=\"table table-striped\">\r\n  <thead>\r\n      <tr>\r\n        <th style=\"text-align: center\" width=\"15%\">No</th>\r\n        <th style=\"text-align: center\" width=\"25%\">음식점</th>\r\n        <th style=\"text-align: center\" width=\"60%\">내용</th>\r\n        <th style=\"text-align: center\" width=\"10%\">삭제</th>\r\n      \r\n      </tr>\r\n    </thead>\r\n    <tbody>\r\n     <tr class=\"reviews\" *ngFor=\"let review of reviews; let i =index\">\r\n        <th style=\"text-align: center\" width=\"15%\"> \r\n          <span class=\"badge\">{{i+1}}</span></th>\r\n        <th style=\"text-align: center\" width=\"25%\">{{review.name}}</th>\r\n        <th style=\"text-align: center\" width=\"60%\">{{review.des}}</th>\r\n        <th style=\"text-align: center\" width=\"10%\"> <input type=\"submit\"  value=\"delete\"></th>\r\n      </tr>\r\n    </tbody>\r\n</table>\r\n\r\n\r\n   &nbsp; <a class=\"btn btn-secondary\">edit</a>\r\n    </form>\r\n    \r\n    \r\n     \r\n      "
 
 /***/ }),
 
@@ -987,7 +987,7 @@ module.exports = "table {\r\n  border-collapse: collapse;\r\n  width: 100%;\r\n}
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<form (submit)=\"deletestores()\">\r\n\r\n<div class=\"container\">\r\n    <h3>Storelist</h3>      \r\n    <table class=\"table table-condensed\" >\r\n      <thead>\r\n        <tr>\r\n          <th style=\"text-align: center\" width=\"10%\">음식점</th>\r\n          <th style=\"text-align: center\" width=\"50%\">대표메뉴</th>\r\n          <th style=\"text-align: center\" width=\"10%\">전화</th>\r\n          <th style=\"text-align: center\" width=\"10%\">주소</th>\r\n          <th style=\"text-align: center\" width=\"20%\">대기자수</th>\r\n          <th style=\"text-align: center\" width=\"10%\">삭제</th>\r\n        </tr>\r\n      </thead>\r\n      <tbody>\r\n        <tr *ngFor=\"let store of stores;\" onMouseOver=\"this.className='bg1'\" onMouseOut=\"this.className='bg2'\">\r\n          <th style=\"text-align: center\" width=\"20%\">{{store.name}}</th>\r\n          <th style=\"text-align: center\" width=\"50%\">{{store.des1}},{{store.des2}},{{store.des3}}</th>\r\n          <th style=\"text-align: center\" width=\"10%\">{{store.call}}</th>\r\n          <th style=\"text-align: center\" width=\"10%\">{{store.add}}</th>\r\n          <th style=\"text-align: center\" width=\"10%\">{{store.count}}</th>\r\n          <th style=\"text-align: center\" width=\"10%\"> <input type=\"submit\"  value=\"delete\"></th>\r\n      \r\n        </tr>\r\n     </tbody>\r\n     </table>\r\n     </div></form>"
+module.exports = "<form (submit)=\"deletestores()\">\r\n\r\n<div class=\"container\">\r\n    <h3>Storelist</h3>      \r\n    <table class=\"table table-condensed\" >\r\n      <thead>\r\n        <tr>\r\n          <th style=\"text-align: center\" width=\"10%\">음식점</th>\r\n          <th style=\"text-align: center\" width=\"50%\">대표메뉴</th>\r\n          <th style=\"text-align: center\" width=\"10%\">전화</th>\r\n          <th style=\"text-align: center\" width=\"10%\">주소</th>\r\n          <th style=\"text-align: center\" width=\"20%\">대기자수</th>\r\n          <th style=\"text-align: center\" width=\"10%\">삭제</th>\r\n        </tr>\r\n      </thead>\r\n      <tbody>\r\n        <tr *ngFor=\"let store of stores;\" onMouseOver=\"this.className='bg1'\" onMouseOut=\"this.className='bg2'\">\r\n          <th style=\"text-align: center\" width=\"10%\">{{store.name}}</th>\r\n          <th style=\"text-align: center\" width=\"50%\">{{store.des1}},{{store.des2}},{{store.des3}}</th>\r\n          <th style=\"text-align: center\" width=\"10%\">{{store.call}}</th>\r\n          <th style=\"text-align: center\" width=\"10%\">{{store.add}}</th>\r\n          <th style=\"text-align: center\" width=\"10%\">{{store.count}}</th>\r\n          <th style=\"text-align: center\" width=\"10%\"> <input type=\"submit\"  value=\"delete\"></th>\r\n      \r\n        </tr>\r\n     </tbody>\r\n     </table>\r\n     </div></form>"
 
 /***/ }),
 
@@ -1165,7 +1165,7 @@ var AuthService = /** @class */ (function () {
         var headers = new _angular_http__WEBPACK_IMPORTED_MODULE_1__["Headers"]();
         headers.append('Authorization', this.authToken);
         headers.append('Content-Type', 'application/json');
-        return this.http.get('http://13.209.244.98:3000/stores/storelist', { headers: headers })
+        return this.http.get('http://localhost:3000/stores/storelist', { headers: headers })
             .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["map"])(function (res) { return res.json(); }));
     };
     AuthService.prototype.getReview = function () {
@@ -1357,7 +1357,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\SY\DatabaseExample\Quiclick\angular-src\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\배혜원\MeanAuthApp-master\angular-src\src\main.ts */"./src/main.ts");
 
 
 /***/ })
